af8954893e9970a10089eafe4bdc1425ee1b708d
MohitMorya2 <mohitmorya56@gmail.com>
2025-07-25 00:24:22 +0530

    frontend connected

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5174",
+    origin: "https://internswallah2.vercel.app/",
     credentials: true,
   })
 );
