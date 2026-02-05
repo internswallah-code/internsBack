9e8ccf916207a45ed23e48cabc98d926b08dc521
Aman kumar <amankumar620282@gmail.com>
2026-02-05 23:05:28 +0530

    Update ApplicationModel.js

--- models/ApplicationModel.js
+++ models/ApplicationModel.js
@@ -4,10 +4,12 @@ const applicationSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
+    required: true,
   },
   jobId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Job",
+    required: true,
   },
   appliedAt: {
     type: Date,
@@ -15,4 +17,6 @@ const applicationSchema = new mongoose.Schema({
   },
 });
 
-export default mongoose.model("Application", applicationSchema);
+const Application = mongoose.model("Application", applicationSchema);
+
+export default Application;
